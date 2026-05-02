489ec1bd0f7032fa5c1cbaaea76f081c8f123c6b
Anna <wjqwjq0926_2025@qq.com>
2026-05-03 02:12:47 +0800

    fix:网页打不开

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,25 @@
+const appManifestPath = './.nuxt/manifest/meta/dev.json'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  experimental: {
+    // This project does not rely on Nuxt app manifest features.
+    // Disabling it avoids unstable internal manifest resolution in Windows dev mode.
+    appManifest: false
+  },
+  alias: {
+    '#app-manifest': appManifestPath
+  },
+  vite: {
+    server: {
+      hmr: {
+        port: 24679
+      }
+    }
+  },
   app: {
     head: {
       htmlAttrs: {
